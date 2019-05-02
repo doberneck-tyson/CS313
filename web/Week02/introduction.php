fffa7a34614711e7287b36662087f320e53e6793
TysonDoberneck <t.j.doberneck@gmail.com>
2019-05-02 16:20:18 -0600

    test

--- web/Week02/introduction.php
+++ web/Week02/introduction.php
@@ -1,7 +1,13 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Tyson
- * Date: 5/2/2019
- * Time: 4:03 PM
- */
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>Test</h1>
+</body>
+</html>
