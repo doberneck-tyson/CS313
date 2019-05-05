8f3e64d8d24af487567602b8949ebb473179c90d
TysonDoberneck <t.j.doberneck@gmail.com>
2019-05-04 21:26:10 -0600

    d

--- web/Week02/introduction.php
+++ web/Week02/introduction.php
@@ -14,7 +14,7 @@
 Galt, CA and I served my mission in the Florida, Tallahassee Mission. My wife and I are expecting our first child mid July and we could
 not be more terrified and excited for anything in our lives. I love to kayak, rock climb and play retro video games in my spare time.</h3>
 
-<img src="/cs313-php/web/images/image.jpg" alt="Wife and I" width="200"
+<img src="./image.jpg" alt="Wife and I" width="200"
      height="80">
 </body>
 </html>
