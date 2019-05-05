91cb8b51193583d512ed8855060d70f042292347
TysonDoberneck <t.j.doberneck@gmail.com>
2019-05-04 21:25:15 -0600

    image add

--- web/Week02/introduction.php
+++ web/Week02/introduction.php
@@ -13,5 +13,8 @@
 <h3>Hello, my name is Tyson Doberneck. I am majoring in Software Engineering and have a year until I graduate. I am originally from
 Galt, CA and I served my mission in the Florida, Tallahassee Mission. My wife and I are expecting our first child mid July and we could
 not be more terrified and excited for anything in our lives. I love to kayak, rock climb and play retro video games in my spare time.</h3>
+
+<img src="/cs313-php/web/images/image.jpg" alt="Wife and I" width="200"
+     height="80">
 </body>
 </html>
