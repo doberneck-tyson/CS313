74d965e473c8ff082e94bd84cbe3d57345e75973
TysonDoberneck <t.j.doberneck@gmail.com>
2019-05-13 13:18:14 -0600

    Rest of code I think

--- web/Week03/confirmation.php
+++ web/Week03/confirmation.php
@@ -2,12 +2,12 @@
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Checkout</title>
+    <title>Purchase Confirmation</title>
     <link rel="stylesheet" type="text/css" href="shoppingCart.css"/>
 </head>
 <body>
 <header id="bHeader">
-    <h1>Checkout</h1>
+    <h1>Purchase Confirmation</h1>
 </header>
 <br/><br/>
 <div class="itemView">
@@ -17,26 +17,21 @@
         foreach($_SESSION['item'] as $value) {
             echo "<h2>$value</h2>" . "<br/>";
         }
-        echo "<h3>Please enter your address:" . "<br/>";
     }
     ?>
     <?php
     $address = "";
-    if(isset($_POST['address'])) {
-        $_SESSION['address'] = $_POST['address'];
+    if(isset($_SESSION['address'])) {
+        $address = $_SESSION['address'];
+        echo "<h2>Shipped to:</h2>" . "<br/>";
+        echo "<h2>$address</h2>" . "<br/>";
+        echo "<h2>Thank you for shopping with us!</h2>";
     }
     ?>
-    <br/>
-    <form action="checkout.php" method="post">
-        <textarea name="address" id="address" rows="3" cols="40"placeholder="street, city, state, zip &#10;Apartment #, PO box"></textarea>
-        <br/>
-        <input type="submit" value="Save address"/>
-    </form>
 </div>
-<br/><br/>
+<br/>
 <div class="flex">
     <button onclick='window.location.href="browseItems.php"' type="button" class="flex-child">Go back to browse page</button>
-    <button onclick='window.location.href="confirmation.php"' type="button" class="flex-child">Complete Purchase</button>
 </div>
 </body>
 </html>
--- web/Week03/viewCart.php
+++ web/Week03/viewCart.php
@@ -1,7 +1,42 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Tyson
- * Date: 5/13/2019
- * Time: 12:33 PM
- */
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<header id="bHeader">
+    <h1>Items Added to Cart</h1>
+</header>
+<br><br>
+<div class="itemView">
+			<?php
+			session_start();
+			if(isset($_SESSION["item"])) {
+					foreach($_SESSION['item'] as $value) {
+					echo "<h2>$value</h2>" . "<br/>";
+				}
+			}
+			else {
+			    echo "<h2>No items selected</h2>" . "<br/>";
+			    echo "<h2>Please return to the browsing page and select the items you would like to add</h2>" . "<br/>";
+			}
+			?>
+</div>
+<br/><br/>
+<div class="flex">
+    <?php
+    if(isset($_SESSION["item"])) {
+        echo ("<button onclick=\"window.location.href= 'browseItems.php'\" type='button' class='flex-child'>Go back to browse page</button>");
+        echo ("<button onclick=\"window.location.href= 'checkout.php'\" type='button' class='flex-child'>Proceed to checkout</button>");
+    }
+    else {
+        echo ("<button onclick=\"window.location.href= 'browseItems.php'\" type='button' class='flex-child'>Go back to browse page</button>");
+    }
+    ?>
+</div>
+</body>
+</html>
