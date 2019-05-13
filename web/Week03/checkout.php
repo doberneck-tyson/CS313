35b7b1be8743ab1c090f9334fe2dc54c26624f87
TysonDoberneck <t.j.doberneck@gmail.com>
2019-05-13 13:34:49 -0600

    Random things

--- web/Week03/checkout.php
+++ web/Week03/checkout.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Checkout</title>
+    <title>Checkout | Tyson's Retro Supply</title>
     <link rel="stylesheet" type="text/css" href="shoppingCart.css"/>
 </head>
 <body>
--- web/Week03/confirmation.php
+++ web/Week03/confirmation.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Purchase Confirmation</title>
+    <title>Confirm | Tyson's Retro Supply</title>
     <link rel="stylesheet" type="text/css" href="shoppingCart.css"/>
 </head>
 <body>
