d07f7ecc221a548249698290979df3f6d1100d1b
TysonDoberneck <t.j.doberneck@gmail.com>
2019-05-13 12:59:33 -0600

    made pages

---
+++ web/Week03/browseItems.php
@@ -0,0 +1,17 @@
+<?php
+sessio_start();
+$_SESSION["item"] = $_POST["item"];
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Browse | Tyson's Retro Supply</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ web/Week03/confirm.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tyson
+ * Date: 5/13/2019
+ * Time: 12:33 PM
+ */
