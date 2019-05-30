56a26f588a01b92966725097426e4435f9e86a09
TysonDoberneck <t.j.doberneck@gmail.com>
2019-05-30 16:08:10 -0600

    add index

--- Database/myDb.sql
+++ Database/myDb.sql
@@ -23,14 +23,18 @@ CREATE TABLE Section(
     section_name VARCHAR(100) NOT NULL,
     section_description VARCHAR(100) NOT NULL
 );
-
-
+INSERT INTO Section (section_name, section_description) VALUES ('Horror' , 'Post your horror stories here.'),('Fantasy', 'Anything similar to LOTR'),('Non-Fiction', 'Did it happen to you? Tell us about it');
+INSERT INTO Section (section_name, section_description) VALUES ('History', 'Anything related to World History you find interesting'),
+('Funny','Will it make me LOL? '),('Campfire','Any stores you feel could be told at a campfire');
 
 CREATE TABLE Comment(
     post_comment VARCHAR(250),
     postcom INT,
     FOREIGN KEY (postcom) REFERENCES Post(post_id)
 );
+
+INSERT INTO Comment (post_comment) VALUES ('THIS IS MY COMMENT');
+
 
 CREATE TABLE Post(
     post_id SERIAL PRIMARY KEY,
@@ -40,3 +44,6 @@ CREATE TABLE Post(
     user_id INT,
     votes INT
 );
+
+INSERT INTO StorytimeAdmin (username,password, display_name) VALUES ('tyson','pass', 'TforTyson');
+INSERT INTO StorytimeUser (username,password,display_name) VALUES ('tyson','pass','TforTyson');
