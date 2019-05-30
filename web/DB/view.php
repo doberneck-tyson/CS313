5f6676ddc4f09d0807e5d23fd3e99347fc593e3e
TysonDoberneck <t.j.doberneck@gmail.com>
2019-05-30 16:32:15 -0600

    add index

--- web/DB/view.php
+++ web/DB/view.php
@@ -1,6 +1,6 @@
 <?php
 
-require "dbConnect.php";
+
 $db = get_db();
 
 ?>
@@ -20,6 +20,27 @@ $db = get_db();
 
 <?php
 
+try
+{
+    $dbUrl = getenv('DATABASE_URL');
+
+    $dbOpts = parse_url($dbUrl);
+
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
+
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+}
 //$statement = $db->prepare("SELECT section_id, section_name, section_description FROM Section");
 //$statement->execute();
 //
