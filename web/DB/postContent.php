2c5b15059399cad3fdf81eb66dd127c475ae3774
TysonDoberneck <t.j.doberneck@gmail.com>
2019-06-09 00:04:22 -0600

    messed with the content, fixing it.

--- web/DB/postContent.php
+++ web/DB/postContent.php
@@ -31,13 +31,14 @@ if(isset($_POST['title'])){
 
 
 //Display content
-//$statement = $db->prepare("SELECT content FROM POST WHERE post_id = " .$_GET['post_id']);
-//$statement->execute();
-//while($row = $statement->fetch(PDO::FETCH_ASSOC))
-//{
-//    $content = $row['content'];
-//    echo "$content";
-//}
+$statement = $db->prepare("SELECT content FROM POST WHERE section_id = " .$_GET['id']);
+$statement->execute();
+
+while($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+    $content = $row['content'];
+    echo "$content";
+}
 
 
 
