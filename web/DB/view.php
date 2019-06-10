f49fd6514d129fdade28d6636b67f95f079a9127
TysonDoberneck <t.j.doberneck@gmail.com>
2019-06-10 16:40:26 -0600

    adding style

--- web/DB/view.php
+++ web/DB/view.php
@@ -1,6 +1,5 @@
 <?php
 
-include("style.css");
 try
 {
     $dbUrl = getenv('DATABASE_URL');
@@ -35,6 +34,8 @@ error_reporting(E_ALL | E_STRICT);
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style.css">
+
     <title>Storytime</title>
 </head>
 <body>
