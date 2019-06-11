1bfac95feb0d4690063c1663abca542017eb076e
TysonDoberneck <t.j.doberneck@gmail.com>
2019-06-11 17:10:05 -0600

    Added login files

--- web/DB/createAccount.php
+++ web/DB/createAccount.php
@@ -11,7 +11,8 @@ error_reporting(E_ALL | E_STRICT);
 $username = $_POST['txtUser'];
 $password = $_POST['txtPassword'];
 if (!isset($username) || $username == ""
-    || !isset($password) || $password == "")
+    || !isset($password) || $password == ""
+    || !isset($display_name) || $display_name == "")
 {
     header("Location: signUp.php");
     die();
@@ -19,16 +20,20 @@ if (!isset($username) || $username == ""
 
 $username = htmlspecialchars($username);
 $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+$display_name = htmlspecialchars($display_name);
 
 require("dbConnect.php");
 $db = get_db();
-$query = 'INSERT INTO StorytimeUser(username, password) VALUES(:username, :password)';
+$query = 'INSERT INTO StorytimeUser(username, password, display_name) VALUES(:username, :password, :display_name)';
 $statement = $db->prepare($query);
 $statement->bindValue(':username', $username);
 
 $statement->bindValue(':password', $hashedPassword);
 $statement->execute();
+
+$statement->bindValue(':display_name', $display_name);
+$statement->execute();
+
 // finally, redirect them to the sign in page
 header("Location: signIn.php");
 die();
-?>
