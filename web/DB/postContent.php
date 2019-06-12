c99d1c2b7d1b6dc92fe5ea633fa76f09f06560e2
TysonDoberneck <t.j.doberneck@gmail.com>
2019-06-11 18:19:59 -0600

    rearrange

--- web/DB/postContent.php
+++ web/DB/postContent.php
@@ -73,7 +73,19 @@ if(isset($_GET["id"])) {
 
 </form>';
 
-$statement = $db->prepare("SELECT post_comment FROM COMMENT WHERE comment_id = " .$_GET['post_id'] );
+
+//Display content DO NOT CHANGE ANYTHING
+$statement = $db->prepare("SELECT content FROM POST WHERE post_id = " .$_GET['post_id']);
+$statement->execute();
+while($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+    $content = $row['content'];
+    echo "$content";
+}
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+    $statement = $db->prepare("SELECT post_comment FROM COMMENT WHERE comment_id = " .$_GET['post_id'] );
 $statement->execute();
 
     while($row = $statement->fetch(PDO::FETCH_ASSOC))
@@ -86,12 +98,3 @@ $statement->execute();
 }
 
 
-//Display content DO NOT CHANGE ANYTHING
-$statement = $db->prepare("SELECT content FROM POST WHERE post_id = " .$_GET['post_id']);
-$statement->execute();
-while($row = $statement->fetch(PDO::FETCH_ASSOC))
-{
-    $content = $row['content'];
-    echo "$content";
-}
-//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
